0796e2d073011c23662cace7d6aa62a6a1727693
Josemi <josemiguel.cabrera312@gmail.com>
2023-04-20 17:09:12 +0200

    Añadido hashcode a ejercicio7-clientes

--- Code/javaDAWa/src/com/colecciones/boletin1/ejercicio7/Cliente.java
+++ Code/javaDAWa/src/com/colecciones/boletin1/ejercicio7/Cliente.java
@@ -1,5 +1,7 @@
 package com.colecciones.boletin1.ejercicio7;
 
+import java.util.Objects;
+
 public class Cliente {
 
 	private int codCliente;
@@ -12,6 +14,27 @@ public class Cliente {
 
 	public int getCodCliente() {
 		return codCliente;
+	}
+
+	
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codCliente);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean iguales = this == obj;
+		
+		if (obj!=null && !iguales && (obj instanceof Cliente c)) {
+			iguales = codCliente == c.codCliente;
+		}
+		
+		return iguales;
 	}
 
 
